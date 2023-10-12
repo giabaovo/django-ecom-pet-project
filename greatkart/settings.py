4929f715ae1e08876b41e5c1b47a854172f2c704
Bao Vo <giabaovo123456@gmail.com>
2023-10-12 11:16:12 +0700

    Edit Settings For Deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    "shop/static",
+    "greatkart/static",
 ]
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.Account'
