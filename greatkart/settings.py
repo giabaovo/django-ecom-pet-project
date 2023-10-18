334aab1a0cca878c44e4d632dbd3f9fb51ae7ea5
Bao Vo <giabaovo123456@gmail.com>
2023-10-18 11:59:08 +0700

    Add Cname to allow host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ["django-greatkart-env.eba-ethvkmue.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["django-greatkart-env-1.eba-4qqdpvfp.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
